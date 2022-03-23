ad21cb5b6d6c96a535fe56211ff2239e57f57f4e
rbennett1435 <89091370+rbennett1435@users.noreply.github.com>
2022-03-23 14:50:35 -0500

    Navbar, register, login, and logo

---
+++ app/views.py
@@ -0,0 +1,48 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+
+from .forms import CreateUserForm
+
+# Create your views here.
+
+def registerPage (request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user=form.cleaned_data.get('username')
+            messages.success(request, "Account was created for " + user)
+            return redirect("login")
+    context = {"form": form}
+    return render(request, "register.html", context)
+
+def indexPage(request):
+    context = {}
+    return render(request, "index.html", context)
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method== "POST":
+            username =request.POST.get("username")
+            password =request.POST.get("password")
+            user =authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                messages.info(request, 'User OR password is incorrect')
+                return render(request, "login.html")
+    context = {}
+    return render(request, "login.html", context)
+
+def eventsPage(request):
+    context = {}
+    return render(request, "events.html", context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
---
+++ config/urls.py
@@ -0,0 +1,29 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoconfig.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app.views import *
+
+
+urlpatterns = [
+    path("register/", registerPage, name="register"),
+    path('admin/', admin.site.urls),
+    path('login/', loginPage, name="login"),
+    path("", indexPage, name="index" ),
+    path("events/", eventsPage, name="events"),
+    path("logout/", logoutUser, name="logout")
+
+]
