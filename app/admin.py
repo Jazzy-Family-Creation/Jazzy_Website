f77f8e479d4081258a8ce741d7092983d9e7e71d
Michelle <mrankin22@basecampcodingacademy.org>
2022-04-18 16:08:30 -0500

    base calender redone

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from app.models import Event
+from app.models import Event, Venue, Users
+
 # Register your models here.
 admin.site.register(Event)
+admin.site.register(Venue)
+admin.site.register(Users)
--- app/models.py
+++ app/models.py
@@ -1,11 +1,55 @@
 from django.db import models
 from datetime import datetime
 
+# class Event(models.Model):
+#     type = models.CharField(max_length=200, null=True)
+#     theme = models.CharField(max_length=200, null=True)
+#     people = models.IntegerField()
+#     location = models.CharField(max_length=200, null=True)
+#     select_date = models.DateField(auto_now=False)
+#     client = models.CharField(max_length=200, null=True)
+    
+class Venue(models.Model):
+    name = models.CharField("Venue Name", max_length=200),
+    address = models.CharField(max_length=300),
+    zip_code = models.CharField('Zip Code', max_length=15),
+    phone = models.CharField('Contact Phone', max_length=25),
+    web = models.URLField('Website Address'),
+    email_address = models.EmailField('Email Address', max_length=120),
+    
+    
+    def __str__(self):
+        return self.name
+   
+   
 
+   
 class Event(models.Model):
-    type = models.CharField(max_length=200, null=True)
-    theme = models.CharField(max_length=200, null=True)
-    people = models.IntegerField()
-    location = models.CharField(max_length=200, null=True)
-    select_date = models.DateField(auto_now=False)
-    client = models.CharField(max_length=200, null=True)
+    type = models.CharField('Type of Event', max_length=200, null = True),
+    theme = models.CharField(max_length=200, null=True),
+    select_date = models.DateTimeField('Event Date'),
+    venue = models.ForeignKey(Venue, blank = True, null=True, on_delete=models.CASCADE)
+    people = models.IntegerField(null=True)
+    client = models.CharField('Client Name', max_length=400),
+
+    
+    def __str__(self):
+        return self.client
+    
+class Users(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField('User Email', max_length=120)
+    number = models.CharField(max_length=30)
+    events = models.ForeignKey(Event, blank = True, null = True, on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+    
+    
+ 
+    
+    
+    
+    
