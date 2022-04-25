35efde6faef0e5482557662f6cc417ffbbbbc3a0
Benc49 <89161459+Benc49@users.noreply.github.com>
2022-04-25 09:24:26 -0500

    fixed settings

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,6 @@ from django.forms import ModelForm
 from .models import Event
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from bootstrap_datepicker_plus import DatePickerInput
 
 
 class CreateUserForm(UserCreationForm):
@@ -12,13 +11,13 @@ class CreateUserForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-class ToDoForm(forms.Form):
-        date = forms.DateField(
-            widget=DatePickerInput(
-                options={
-                    "format": "mm/dd/yyyy",
-                    "autoclose": True
-                }
-            )
-        )
+# class ToDoForm(forms.Form):
+#         date = forms.DateField(
+#             widget=DatePickerInput(
+#                 options={
+#                     "format": "mm/dd/yyyy",
+#                     "autoclose": True
+#                 }
+#             )
+#         )
 
